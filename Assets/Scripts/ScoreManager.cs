d916c5063269051617693f24ed9906ac6ea40aa2
Daniela <d.marino10@uniandes.edu.co>
2020-06-01 17:09:21 -0700

    beginning of scoring

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour
+{
+
+    public static ScoreManager instance;
+
+    public Text scoreText;
+
+    public int nearMissScore=5;
+    public int normalAvoidScore=1;
+    public int asteroidDestroyedScore=2;
+
+    private int score=0;
+
+
+    void NearMiss(){
+        score+= nearMissScore;
+        //scoreText.text = score;
+    }
+
+    void NormalAvoid(){
+        score += normalAvoidScore;
+    }
+
+    void AsteroidDestroyed(){
+        score += asteroidDestroyedScore;
+    }
+}
